4142c504694e6e25525ad3770f7530a622cc00a6
Jared <jared.goo@convoy.com.hk>
2021-05-08 23:30:55 +0800

    friday

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -8,9 +8,8 @@ import * as XLSX from 'xlsx'
 
 function Home(){
     const [headers, setHeaders] = useState([''])
+		const [hasHeader, setHeaderBoolean] = useState(true)
     const [file, setFile] = useState({})
-
-		
 
     const onDrop = useCallback(acceptedFiles => {
       
@@ -33,42 +32,47 @@ function Home(){
 			}
 			return header
 		}
+		const onHasHeaderClick = () =>{
+			setHeaderBoolean(!hasHeader)
+		}
 
     const onFileDrop = (file) =>{
 			console.log(file)
 			let newHeader = [];
-				const reader = new FileReader();
-				console.log(typeof file)
-				reader.readAsBinaryString(file[0])
-				const rABS = !!reader.readAsBinaryString;
-				reader.onload = (e) =>{
-					console.log(e)
-					const bufferArray = e.target.result;
-					const wb = XLSX.read(bufferArray,{type:rABS? "binary": "array"});
-					const wsname= wb.SheetNames[0];
-					const ws = wb.Sheets[wsname];
-					const data = XLSX.utils.sheet_to_json(ws);
-					const header = extractHeader(ws)
-					if(header){
-						console.log('hehe')
-						setHeaders([...header,newHeader])
-					}
-          console.log(header);
-				} 
+			const reader = new FileReader();
+			console.log(typeof file)
+			reader.readAsBinaryString(file[0])
+			const rABS = !!reader.readAsBinaryString;
+			reader.onload = (e) =>{
+				console.log(e)
+				const bufferArray = e.target.result;
+				const wb = XLSX.read(bufferArray,{type:rABS? "binary": "array"});
+				const wsname= wb.SheetNames[0];
+				const ws = wb.Sheets[wsname];
+				const data = XLSX.utils.sheet_to_json(ws);
+				let header = []
+				if(hasHeader){
+					header = extractHeader(ws)
+				}
+				if(header){
+					setHeaders(header)
+				}
+        console.log(header);
+			} 
     }
 
     return (
         <div className="main-container">
 					<div className="upper-container">
             <form className="note-container">
-							<div>
-								<label>Has header? </label>
-								<input type="checkbox"/>
+							<div className="form-group row">
+								<label className="col-3 label form-control">Has header? </label>
+								<input className="col-9 input form-control" type="checkbox" onClick={onHasHeaderClick}/>
 							</div>
-							<div>
+							<div className="file-container">
                 	{headers.map((h, index)=>{
                 	    return (
-                	      <div className="row form-group" key={index}>
+                	      <div className="form-group row" key={index}>
                 	          <label className="col-3 label form-control"> Header {index+1} </label>
                 	          <input className="col-9 input form-control" type='text' value={h}/>
                 	      </div>
@@ -77,14 +81,17 @@ function Home(){
 									<span className='btn btn-primary btn-sm' onClick={addNewHeader}><FaPlus/></span>
 							</div>
             </form>
-            
+						{/* {
+       				(headers.length>1)? <div> hi</div> : <div>bye</div>
+        		} */}
+
             <div className="file-container">
                 <Dropzone onDrop={(file)=>onFileDrop(file)}/>
             </div>
 						</div>
-						<div className="lower-container">
+						{/* <div className="lower-container">
 
-						</div>
+						</div> */}
         </div>
     )
 }
